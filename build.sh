ca47b4b304e5d743c737ea652f6ab15dc8541576
Christian Autermann <c.autermann@52north.org>
2019-10-01 13:06:59 +0200

    use snap:6

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-docker build --pull -t 52north/testbed-eopad-snap:latest snap/
+docker build --pull -t 52north/testbed-eopad-snap:latest -t 52north/testbed-eopad-snap:6 snap/
+docker build -t 52north/testbed-eopad-ndvi:latest ndvi/
+docker build -t 52north/testbed-eopad-quality:latest quality/
+
 docker push 52north/testbed-eopad-snap:latest
-
-docker build --pull -t 52north/testbed-eopad-ndvi:latest ndvi/
+docker push 52north/testbed-eopad-snap:6
 docker push 52north/testbed-eopad-ndvi:latest
-
-docker build --pull -t 52north/testbed-eopad-quality:latest quality/
 docker push 52north/testbed-eopad-quality:latest
 
